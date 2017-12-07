d529005f7ced5d83db2e6bd4ddff3e2d1849f11a
ajpershick <ajpershick@gmail.com>
2017-12-07 15:24:16 -0800

    finished ALOHA

--- Project2/ALOHA.py
+++ Project2/ALOHA.py
@@ -1,5 +1,23 @@
-from random import random
+from random import randint
 
 N = 8
 p = 0.5
+
+slots = 1000000
+successes = 0
+actual = 0
+
+for numslots in range(slots):
+    sum = 0
+    for node in range(N):
+        if randint(0,1) == 1:
+            sum +=1
+    if sum == 1:
+        successes +=1
+        actual = successes / (numslots + 1)
+
+theoretical = (N*p*(1-p)**(N-1))
+
+print("Theoretical = ", theoretical)
+print("Actual = ", actual)
 
